625c6d3436a41bc9bc844627631eef6b9cdb6a50
Luke Jakielaszek <tug52339@temple.edu>
2020-02-12 15:56:49 -0500

    Created clock java classes empty.

---
+++ app/src/main/java/edu/temple/vs_owlnet_chat/LamportClock.java
@@ -0,0 +1,5 @@
+package edu.temple.vs_owlnet_chat;
+
+public class LamportClock {
+    private int time;
+}
---
+++ app/src/main/java/edu/temple/vs_owlnet_chat/VectorClock.java
@@ -0,0 +1,8 @@
+package edu.temple.vs_owlnet_chat;
+
+import java.util.Map;
+
+public class VectorClock{
+    private Map<Integer, Integer> vector;
+
+}
