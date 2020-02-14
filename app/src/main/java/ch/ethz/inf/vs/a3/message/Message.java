8a3393b3cb6e29e7f978d31af402a75a2f231781
Luke Jakielaszek <tug52339@temple.edu>
2020-02-14 11:52:36 -0500

    Created getters & setters for message

--- app/src/main/java/ch/ethz/inf/vs/a3/message/Message.java
+++ app/src/main/java/ch/ethz/inf/vs/a3/message/Message.java
@@ -1,4 +1,51 @@
 package ch.ethz.inf.vs.a3.message;
 
+import ch.ethz.inf.vs.a3.clock.Clock;
+
 class Message {
+    private Clock timestamp;
+    private String username;
+    private String uuid;
+    private String type;
+    private String content;
+
+    public Clock getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Clock timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
